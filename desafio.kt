7b26c184f2dfd61fc179682f0ac78e24ba2fe274
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-12-06 20:26:55 -0300

    Evolução da abstração

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,48 @@
+import jdk.jfr.Percentage
+
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+@JvmInline
+value class Porcentagem(val valor: Double)
 
-class Usuario
+@JvmInline
+value class Minutos(val valor: Double)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Usuario(
+    val nome: String,
+    var idade: Int,
+) {
+    val progressos = mutableMapOf<Formacao, Porcentagem>()
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(
+    val nome: String,
+    val duracao: Minutos,
+)
+
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+    val duracao: Porcentagem
+        get() {
+            TODO("Use a lista de $conteudos para calcular a duração total da formação, em minutos")
+        }
+
     fun matricular(usuario: Usuario) {
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        TODO("Atualize o atributo progressos do $usuario, adicionando a formação")
+    }
+
+    fun concluirConteudo(usuario: Usuario, indiceConteudo: Int) {
+        TODO("Atualize o atributo progressos para a formação do usuário com base na:" +
+            "Duração do conteúdo concluído" +
+            "Progresso atual do usuário na formação" +
+            "Duração total da formação"
+        )
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
