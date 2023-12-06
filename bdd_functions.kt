a67671900c77067e6cb3dac44de0141ea97e9c2f
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-12-06 19:29:28 -0300

    criadas funções para bdd

---
+++ bdd_functions.kt
@@ -0,0 +1,7 @@
+/*
+* Funções úteis para auxiliar na estruturação de testes
+*/
+
+inline fun <T> given(function: () -> T): T = function()
+inline infix fun <T, R> T.whenDone(function: T.() -> R): R = function()
+inline infix fun <T> T.then(function: (T) -> Unit): Unit = function(this)
