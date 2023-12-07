0e7ef9bdc057bd59b97526cbfe104ab689e3d766
FSLucas22 <lucassantos@presconinformatica.com.br>
2023-12-06 23:05:23 -0300

    Ajustes

--- tests/test_utilities/bdd_functions.kt
+++ tests/test_utilities/bdd_functions.kt
@@ -4,6 +4,12 @@ package tests.test_utilities
 * Funções úteis para auxiliar na estruturação de testes
 */
 
-inline fun <T> given(function: () -> T): T = function()
-inline infix fun <T, R> T.whenDone(function: T.() -> R): R = function()
-inline infix fun <T> T.then(function: (T) -> Unit): Unit = function(this)
+data class Context<T>(val context: T) {
+    inline infix fun <R> whenDone(function: T.() -> R): Result<T, R> = Result(context, function(context))
+}
+
+data class Result<T, R>(val context: T, val result: R) {
+    inline infix fun then(function: T.(R) -> Unit): Unit = function(context, result)
+}
+
+inline fun <T> given(function: () -> T): Context<T> = Context(function())
